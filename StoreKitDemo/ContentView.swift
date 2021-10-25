a77295877d8f908a4450718b9c69bd7950c4e1be
Eugen Soloviov <suenot@gmail.com>
2021-10-25 23:07:59 +0300

    init empry project

---
+++ StoreKitDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StoreKitDemo
+//
+//  Created by Eugen Soloviov on 25.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StoreKitDemo/StoreKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKitDemoApp.swift
+//  StoreKitDemo
+//
+//  Created by Eugen Soloviov on 25.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct StoreKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
